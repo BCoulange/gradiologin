2d6d1568bc199620bbcda9eb90bcfa8861f9fc7d
Baptiste Coulange <baptiste.coulange@gmail.com>
2025-10-03 18:46:38 +0200

    Update __init__.py

--- gradiologin/__init__.py
+++ gradiologin/__init__.py
@@ -2,7 +2,3 @@ from gradiologin.functions import get_user, LogoutButton
 from gradiologin.register import register, mount_gradio_app
 import logging
 
-logger = logging.getLogger(__name__)  # NOT root
-
-# usage
-logger.info("Starting login flow")
